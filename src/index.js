721a302d9891f643668fd7974774fb3696bad923
Binhnn <binhbon1122@gmail.com>
2020-09-01 10:54:28 +0700

    handle read and write file eslint

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('Hello world');
+const inquirer = require("inquirer");
+const { writeFileSync } = require("fs");
+
+const eslintReact = require('./config/eslint.react.json');
+
+const configFiles = {
+	react: eslintReact,
+};
+
+(async () => {
+	const { framework } = await inquirer.prompt([
+		{
+			type: 'list',
+			message: 'Pick a framework.',
+			name: 'framework',
+			choices: Object.keys(configFiles),
+		}
+	]);
+
+	const cwd = process.cwd();
+
+	await writeFileSync(`${cwd}/eslint.json`, JSON.stringify(configFiles[framework], null, 2));
+
+	console.log('Successfully!');
+})();
